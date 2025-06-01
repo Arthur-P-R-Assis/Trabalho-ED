956ea04a5e37a89acebd062afdb1eb217fdb2cc4
Arthur <133927449+Arthur-P-R-Assis@users.noreply.github.com>
2025-06-01 17:07:32 -0300

    bd_paciente.h

--- bd_paciente.h
+++ bd_paciente.h
@@ -1,11 +1,13 @@
 #ifndef BD_PACIENTE_H
 #define BD_PACIENTE_H
 
+// define o tamanho máximo do vetor de pacientes e os tamanhos das strings
 #define MAX_PACIENTES 1000
 #define TAM_NOME 100
 #define TAM_CPF 20
 #define TAM_DATA 11
 
+// struct que guarda os dados de um paciente
 typedef struct {
     int id;
     char cpf[TAM_CPF];
@@ -14,11 +16,13 @@ typedef struct {
     char data_cadastro[TAM_DATA];
 } Paciente;
 
+// struct que guarda o vetor de pacientes e o total
 typedef struct {
     Paciente pacientes[MAX_PACIENTES];
     int total_pacientes;
 } BancoPacientes;
 
+// funções que o programa usa
 void carregar_banco(BancoPacientes *banco, const char *arquivo);
 void imprimir_lista_pacientes(BancoPacientes *banco);
 void consultar_paciente(BancoPacientes *banco);
