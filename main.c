e84726c26451165551329db7d829d84fc0fc5fc6
Arthur <133927449+Arthur-P-R-Assis@users.noreply.github.com>
2025-06-01 17:05:42 -0300

    main.c

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include "bd_paciente.h"
 
+// Função do menu principal
 void menu(BancoPacientes *banco) {
     char opcao;
     char auxiliar;
 
-    auxiliar =0;
+    auxiliar = 0; // variável para controlar o loop
 
-    while (auxiliar != 'S' && auxiliar != 's'){
+    // loop do menu até o usuário escolher sair
+    while (auxiliar != 'S' && auxiliar != 's') {
         printf("\nHealthSys - Sistema de Pacientes\n");
         printf("1 - Consultar paciente\n");
         printf("2 - Imprimir lista de pacientes\n");
@@ -15,21 +17,18 @@ void menu(BancoPacientes *banco) {
         printf("Escolha: ");
         scanf(" %c", &opcao);
 
-        auxiliar = opcao;
+        auxiliar = opcao; // guarda a escolha do usuário
 
         if (opcao == '1') {
-            consultar_paciente(banco);
+            consultar_paciente(banco); // chama a função de consultar
         } 
-        
         else if (opcao == '2') {
-            imprimir_lista_pacientes(banco);
+            imprimir_lista_pacientes(banco); // chama a função de imprimir lista
         } 
-
         else if (opcao == 'S' || opcao == 's') {
             printf("Saindo...\n");
-            break;
+            break; // sai do loop
         } 
-        
         else {
             printf("Opção inválida\n");
         }
@@ -37,8 +36,8 @@ void menu(BancoPacientes *banco) {
 }
 
 int main() {
-    BancoPacientes banco;
-    carregar_banco(&banco, "bd_paciente.csv");
-    menu(&banco);
-    return 0;
+    BancoPacientes banco; // declara a variável para guardar os pacientes
+    carregar_banco(&banco, "bd_paciente.csv"); // carrega os pacientes do arquivo
+    menu(&banco); // chama o menu principal
+    return 0; // finaliza o programa
 }
